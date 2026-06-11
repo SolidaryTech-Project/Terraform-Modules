b61402851c8a6ee17d000d59704c23194b4ba222
Lucas <morganis1411@gmail.com>
2026-06-10 21:20:22 -0300

    feat: updating eks cluster

--- Modules/eks-cluster/cluster.tf
+++ Modules/eks-cluster/cluster.tf
@@ -17,6 +17,11 @@ resource "aws_eks_cluster" "this" {
     endpoint_public_access  = var.endpoint_public_access
   }
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   depends_on = [aws_iam_role_policy_attachment.cluster]
 
   tags = merge(var.tags, {
